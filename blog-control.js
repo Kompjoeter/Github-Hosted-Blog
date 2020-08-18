d61a59cdb565ebfa14acd890ed2a3f8c4e9ef6a3
RanDByyp <47929945+RanDByyp@users.noreply.github.com>
2020-08-18 12:35:37 +0200

    Fixed link to json file

--- blog-control.js
+++ blog-control.js
@@ -1,6 +1,6 @@
 var postAmount;
 var postActive;
-var postsSource = "https://raw.githubusercontent.com/RanDByyp/randbyyp.github.io/master/posts.json"
+var postsSource = "https://github.com/RanDByyp/Github-Stored-Blog/blame/master/posts.json"
 
 window.addEventListener('load', (event) => 
 {
